18961dff03058b878be34e2ad35c7380b9c61fa3
noahmac-tech <noahmccahill123@gmail.com>
2025-11-23 14:43:30 +0000

    created number_of_localized_states and localized_analytical_energies functions.

--- functions.py
+++ functions.py
@@ -13,6 +13,21 @@ def q_parameter(p):
 def potential_energy(x,q):
     return - 1 / (np.cosh(q * x) ** 2)
 
+def number_of_localized_states(p):
+    s = 0.5 * (np.sqrt(1 + 4 / q_parameter(p)**2) - 1)
+    return int(s)
+
+def localized_analytical_energies(p,n):
+    s = number_of_localized_states(p)
+    energies_analytical = []
+    for n in range(int(s)):
+        E_n = -(1/(4*p)) * (-(1 + 2*n) + np.sqrt(1 + 4*p))**2
+        if E_n < 0: 
+            energies_analytical.append(E_n)
+    return energies_analytical
+
+    
+
 def stationary_hamiltonian(N,L,p):
 
     main_diag = 2 / dx**2 + potential_energy(xi,q_parameter(p))
