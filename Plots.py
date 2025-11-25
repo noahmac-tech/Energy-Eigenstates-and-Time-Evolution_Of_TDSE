b9dcb91d9fdba40b327f373be2fcfd708ed11778
noahmac-tech <noahmccahill123@gmail.com>
2025-11-25 12:57:36 +0000

    Made plot 4 N=100, L =150

--- Plots.py
+++ Plots.py
@@ -55,3 +55,21 @@ ax1.set_xlabel('$\u03BE$')
 ax1.set_ylabel('u($\u03BE$)')
 ax1.legend()
 plt.show()
+
+# Plot 4: N = 100, L = 150, P = 30
+
+xi, dx = Grid(L=150, N=100)
+H = stationary_hamiltonian(N=100, L=150, p=30, dx=dx, xi=xi)
+energies, wavefunctions = eigenvalues_and_vectors(H)
+bound_energies = Bound_energies(energies)
+bound_wavefunctions = Bound_wavefuncs(wavefunctions, energies)
+
+fig1 = plt.figure(figsize=(10, 6))
+ax1 = fig1.add_subplot(1, 1, 1)
+for i, psi in enumerate(bound_wavefunctions.T):
+    psi_norm = psi / np.sqrt(np.trapezoid((np.abs(psi)**2), xi))  # Normalizing the wavefunction
+    ax1.plot(xi, psi_norm, label=f'n={i}, E={bound_energies[i]:.3f}')
+ax1.set_xlabel('$\u03BE$')
+ax1.set_ylabel('u($\u03BE$)')
+ax1.legend()
+plt.show()
